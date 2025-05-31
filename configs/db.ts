e25fa8554a86aa6337c7fd07b335f05a2a3d7765
karthik <karthiknadar1204@gmail.com>
2025-05-31 09:04:06 +0530

    build fixes #1

--- configs/db.ts
+++ configs/db.ts
@@ -2,7 +2,11 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from "./schema"
 
+if (!process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL) {
+  throw new Error('NEXT_PUBLIC_DRIZZLE_DATABASE_URL is not defined');
+}
+
 const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL);
-export const db = drizzle(sql,{schema});
+export const db = drizzle(sql, { schema });
 
 // const result = await db.select().from(...);
